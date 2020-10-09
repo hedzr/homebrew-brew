4ee0fbeb2c2965ffdad53acfc85e453f19ce6ac9
Hedzr Yeh <hedzrz@gmail.com>
2020-10-09 21:59:42 +0800

    solved.

--- Formula/awesome-tool.rb
+++ Formula/awesome-tool.rb
@@ -4,6 +4,7 @@ class AwesomeTool < Formula
   url "https://github.com/hedzr/awesome-tool/releases/download/v1.1.8/awesome-tool-v1.1.8-darwin-amd64.tgz"
   version "1.1.8"
   sha256 "b6aa3f3105063bbbfe600096486daadca9530ba351839c8252a01881337240e9"
+  license "MIT"
 
   def install
     bin.install "awesome-tool"
