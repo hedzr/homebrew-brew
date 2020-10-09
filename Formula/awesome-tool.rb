8d18fe19ea6d787322452fb65e60dfed92103239
Hedzr Yeh <hedzrz@gmail.com>
2020-10-09 17:12:20 +0800

    fixed the download url in awesome-tool.rb manually

--- Formula/awesome-tool.rb
+++ Formula/awesome-tool.rb
@@ -1,7 +1,7 @@
 class AwesomeTool < Formula
   desc "It is a CLI tool to retrieve the stars of all repos in an awesomeness-list"
   homepage "https://github.com/hedzr/awesome-tool"
-  url "https://github.com/hedzr/awesome-tool/releases/download/v#{version}/awesome-tool-v#{version}-darwin-amd64.gz"
+  url "https://github.com/hedzr/awesome-tool/releases/download/v1.1.5/awesome-tool-v1.1.5-darwin-amd64.tgz"
   version "1.1.5"
   sha256 "6fa71bd688a619869694687e745c8ca596f868604b774f14eb07cb0704276d6c"
 
