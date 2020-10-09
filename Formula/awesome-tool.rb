31dead928675b3970e59c1b6a8e927f37236d5e5
Hedzr Yeh <hedzrz@gmail.com>
2020-10-09 15:53:08 +0800

    fixes made

--- Formula/awesome-tool.rb
+++ Formula/awesome-tool.rb
@@ -1,7 +1,7 @@
 class AwesomeTool < Formula
   desc "It is a CLI tool to retrieve the stars of all repos in an awesomeness-list"
   homepage "https://github.com/hedzr/awesome-tool"
-  url "https://github.com/hedzr/awesome-tool/releases/download/v1.1.1/awesome-tool.gz"
+  url "https://github.com/hedzr/awesome-tool/releases/download/v#{version}/awesome-tool-v#{version}-darwin-amd64.gz"
   version "1.1.1"
   sha256 "2b715af5a5a1d10d920fbdb5fa0c47925a9047e292b11dee891d2bc14371f943"
 
