f47d563c56e984263b504113183409d26904ac0b
hz <hedzrz@gmail.com>
2020-10-27 08:49:59 +0000

    awesome-tool 1.1.10

--- Formula/awesome-tool.rb
+++ Formula/awesome-tool.rb
@@ -1,9 +1,9 @@
 class AwesomeTool < Formula
   desc "It is a CLI tool to retrieve the stars of all repos in an awesomeness-list"
   homepage "https://github.com/hedzr/awesome-tool"
-  url "https://github.com/hedzr/awesome-tool/releases/download/v1.1.9/awesome-tool-v1.1.9-darwin-amd64.tgz"
-  version "1.1.9"
-  sha256 "7ddcc928cbdbb0924e7b93aed9ee749be79f32ec6bcdae4a71ea797665f5e83f"
+  url "https://github.com/hedzr/awesome-tool/releases/download/v1.1.10/awesome-tool-v1.1.10-darwin-amd64.tgz"
+  version "1.1.10"
+  sha256 "2ea92b0a13ebaacfa3908447e0cf706ccfc96e7efe3d353c08e76ed54e6b3734"
   license "MIT"
 
   def install
@@ -12,4 +12,4 @@ class AwesomeTool < Formula
   end
 end
 
-# https://github.com/hedzr/awesome-tool/releases/download/v1.1.9/awesome-tool-v1.1.9-darwin-amd64.tgz
+# https://github.com/hedzr/awesome-tool/releases/download/v1.1.10/awesome-tool-v1.1.10-darwin-amd64.tgz
