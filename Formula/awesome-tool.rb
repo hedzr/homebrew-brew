7dd063701919b665f8e628c2d475adb3012069c6
Hedzr Yeh <hedzrz@gmail.com>
2021-07-14 00:26:39 +0800

    updated

--- Formula/awesome-tool.rb
+++ Formula/awesome-tool.rb
@@ -6,7 +6,7 @@ class AwesomeTool < Formula
   # desc "It is a CLI tool to retrieve the stars of all repos in an awesomeness-list"
   homepage "https://github.com/hedzr/awesome-tool"
   url "https://github.com/hedzr/awesome-tool/releases/download/v1.1.11/awesome-tool-v1.1.11-darwin-amd64.tgz"
-  version "v1.1.11"
+  version "1.1.11"
   sha256 "027594a8e005ea6b893a544a18e5c98c8c152e80c52a17182d00a20057d99ad7"
   license "MIT"
 
--- Formula/consul-tags.rb
+++ Formula/consul-tags.rb
@@ -5,7 +5,7 @@ class ConsulTags < Formula
   desc "It is a CLI tool to update, add, remove the service tags at consul store"
   homepage "https://github.com/hedzr/consul-tags"
   url "https://github.com/hedzr/consul-tags/releases/download/v0.6.1/consul-tags-v0.6.1-darwin-amd64.tgz"
-  version "v0.6.1"
+  version "0.6.1"
   sha256 "8a2ba08f649a23b38417d978346f2a0dff7a394257ca15685053730963260a26"
   license "MIT"
 
