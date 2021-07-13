0b67097e8d84a230be8a45e96071fb4ac9c37a0c
Hedzr Yeh <hedzrz@gmail.com>
2021-07-13 22:45:57 +0800

    upgrade consul-tags

--- Formula/consul-tags.rb
+++ Formula/consul-tags.rb
@@ -1,9 +1,9 @@
 class ConsulTags < Formula
   desc "It is a CLI tool to update, add, remove the service tags at consul store"
   homepage "https://github.com/hedzr/consul-tags"
-  url "https://github.com/hedzr/consul-tags/releases/download/v0.5.9/consul-tags-v0.5.9-darwin-amd64.tgz"
-  version "0.5.9"
-  sha256 "0bfe06134b58ba031fd321564aebb71ac8dc0e0258881c6b906810b675c7c790"
+  url "https://github.com/hedzr/consul-tags/releases/download/v0.6.1/consul-tags-v0.6.1-darwin-amd64.tgz"
+  version "0.6.1"
+  sha256 "8a2ba08f649a23b38417d978346f2a0dff7a394257ca15685053730963260a26"
   license "MIT"
 
   def install
