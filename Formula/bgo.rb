0afd9056e6d82fd0cb2aed9f7d98855bb9b70ad3
hz <hedzrz@gmail.com>
2022-01-30 09:40:33 +0800

    imp: bgo install scripts

--- Formula/bgo.rb
+++ Formula/bgo.rb
@@ -28,6 +28,12 @@ class Bgo < Formula
       def install
         bin.install "./bgo"
         # etc.install Dir["etc/*"]
+        system "./bgo", "gen", "sh", "--bash", "-o", "bgo.bash"
+        system "./bgo", "gen", "sh", "--zsh", "-o", "bgo.zsh"
+        system "./bgo", "gen", "man", "-d", "man1"
+        zsh_completion.install "./bgo.zsh" => "_bgo"
+        bash_completion.install "./bgo.bash" => "bgo"
+        #man1.install "bgo.1"
       end
     end
   end
